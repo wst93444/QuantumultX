21490f6136c09eb52585e179f6a2e7ea3072529b
wst93444 <60587653+wst93444@users.noreply.github.com>
2021-05-16 01:57:26 -0700

    Create jd_finance_cookie.js

---
+++ Rewrite/jd_finance_cookie.js
@@ -0,0 +1,3 @@
+hostname=ms.jr.jd.com
+
+^https?:\/\/ms\.jr\.jd\.com\/gw\/generic\/uc\/newna\/m\/userstat  url script-request-body https://raw.githubusercontent.com/dompling/Script/master/jd/jd_jr_cookie.js
