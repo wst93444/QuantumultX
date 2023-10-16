bd1e3451079d777e91a63a5338eb538e654420da
wst93444 <60587653+wst93444@users.noreply.github.com>
2023-10-16 01:41:55 -0700

    Update Adblock4limbo.js

--- list/Adblock4limbo.js
+++ list/Adblock4limbo.js
@@ -1,5 +1,5 @@
 const regex='<head>';const replace_str='<head>\
 <link rel="stylesheet" href="https://limbopro.com/CSS/Adblock4limbo.user.css" type="text/css" />\
-<script type="text/javascript" async="async" src="https://raw.githubusercontent.com/wst93444/QuantumultX/master/list/Adblock4limbo.user(1).js"></script>\
+<script type="text/javascript" async="async" src="https://raw.githubusercontent.com/wst93444/QuantumultX/master/list/Adblock4limbo.user.js"></script>\
 '
 if($response.body){var body=$response.body.replace(regex,replace_str)};const headers=$response.headers;headers['Content-Security-Policy']='*';$done({headers:headers,body:body});
